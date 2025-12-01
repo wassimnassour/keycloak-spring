66eab6782e9fee683c178583f02c79825b8e762a
Wassim NASSOUR <wassim.nassour@yakeey.com>
2025-12-01 11:54:41 +0100

    setup project 🏋🏼‍♂️

---
+++ src/main/java/com/security/keycloak_app/KeycloakAppApplication.java
@@ -0,0 +1,13 @@
+package com.security.keycloak_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/keycloak_app/KeycloakAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.keycloak_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
